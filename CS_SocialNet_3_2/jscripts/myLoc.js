26d04da75714f801b670924ad7f12fdb3531f993
NickHamilton01 <nah654@mun.ca>
2016-10-30 23:18:39 -0230

    Code to add marker, but there is an issue
    The rest of the script stops working when the addMarker funtion is in
    the code, but if you remove that it continues to work fine. I can't
    figure out what part of it is causing problems with the map

--- CS_SocialNet_3_2/jscripts/myLoc.js
+++ CS_SocialNet_3_2/jscripts/myLoc.js
@@ -13,8 +13,10 @@ function setMap() {
 }
 
 function handleButtonClick() { var x = document.getElementById("tbody")
+	var map = document.getElementById("map");
 	var friend = document.getElementById("friendSelect").value;
     var location = document.getElementById("locationSelect").value;
+    var date = new Date();
     
 	if (tbody.rows.length > 0) {
 		for (var i = 0; i < tbody.rows.length; i++) {
@@ -24,15 +26,46 @@ function handleButtonClick() { var x = document.getElementById("tbody")
 		var row = x.insertRow(0);
 		var nameCell = row.insertCell(0);
         var locCell = row.insertCell(1);
+        var dateCell = row.insertCell(2);
         nameCell.innerHTML = friend;
         locCell.innerHTML = location;
+        dateCell.innerHTML = date.getDay()+"/"+date.getMonth()+"/"+date.getFullYear()+" at "+date.getHours()+":"+date.getMinutes();
 	}
 	
     else {
         var row = x.insertRow(-1);
         var nameCell = row.insertCell(0);
         var locCell = row.insertCell(1);
+        var dateCell = row.insertCell(2);
         nameCell.innerHTML = friend;
         locCell.innerHTML = location;
+        dateCell.innerHTML = date.getDay()+"/"+date.getMonth()+"/"+date.getFullYear()+" at "+date.getHours()+":"+date.getMinutes();
+        
+        var content = friend+" was here on"+dateCell.innerHTML;
+        
+        addMarker(map, friend, content, location)
     }
+}
+
+function addMarker(map, title, content, location) {
+	if (location == "MUN") {var ltln = new google.maps.LatLng(47.572479, -52.733879)}
+	else if (location == "Avalon Mall") {var ltln = new google.maps.LatLng(47.560972, -52.755122)}
+	else if (location == "Downtown") {var ltln = new google.maps.LatLng(47.561725, -52.709991)}
+	else (location == "Village Mall") {var ltln = new google.maps.LatLng(47.534963, -52.750613)}
+	
+	var markerOptions = {
+			position: ltln,
+			map: map,
+			title: title,
+			clickable: true
+	}
+	var marker = new google.maps.Marker(markerOptions); 
+	
+    var infoWindowOptions = {
+    	content: content,
+    	position: ltln
+    }
+    var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+    google.maps.event.addListener(marker, "click", function() {infoWindow.open(map);});
 }
